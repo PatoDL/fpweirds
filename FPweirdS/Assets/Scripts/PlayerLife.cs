b030b1a9c2e7a285364dad34f86bf1f1366c95a3
Pato <patodl2701@gmail.com>
2019-04-14 05:26:51 -0300

    added trigger check on traps

--- FPweirdS/Assets/Scripts/PlayerLife.cs
+++ FPweirdS/Assets/Scripts/PlayerLife.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class PlayerLife : MonoBehaviour
 {
+    public GameObject player;
     float vida;
+    public float force;
+    Rigidbody rig;
     // Start is called before the first frame update
     void Start()
     {
         vida = 100;
+        rig = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -17,8 +21,13 @@ public class PlayerLife : MonoBehaviour
         
     }
 
-    void OnCollisionEnter(Collision col)
+    private void OnTriggerEnter(Collider col)
     {
-        Debug.Log(col.gameObject.name);
+        if (col.gameObject.name == "Trap")
+        {
+            vida -= 50;
+            rig.AddForce(Vector3.back * force);
+            Debug.Log("life: " + vida);
+        }
     }
 }
