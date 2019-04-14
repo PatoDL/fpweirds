325bd767e7e3be06941b3ca8ed5ed7438eb35a5f
Pato <patodl2701@gmail.com>
2019-04-14 04:25:34 -0300

    fps camera added with some testing objects

---
+++ FPweirdS/Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLife : MonoBehaviour
+{
+    float vida;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vida = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log(col.gameObject.name);
+    }
+}
---
+++ FPweirdS/Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneManagement : MonoBehaviour
+{
+    string scene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scene = "Game";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
